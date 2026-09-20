ecd97f4c0835058a44c3ed353c43ea16be9e7da2
Victor Kovalchuk <vict02r05@gmail.com>
2026-09-20 09:56:56 +0300

    introducing components

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const app = Vue.createApp({
   data() {
     return {
-      detailsAreVisible: false,
       friends: [
         { 
           id: 'manuel', 
@@ -16,6 +15,32 @@ const app = Vue.createApp({
           email: 'julie@localhost.com' 
         }
       ]
+    }
+  }
+});
+
+app.component('friend-contact', {
+  template: `
+    <li>
+      <h2>{{ friend.name }}</h2>
+      <button @click="toggleDetails">
+        {{ detailsAreVisible ? 'Hide' : 'Show' }} Details
+      </button>
+      <ul v-if="detailsAreVisible">
+        <li><strong>Phone:</strong>{{ friend.phone }}</li>
+        <li><strong>Email:</strong>{{ friend.email }}</li>
+      </ul>
+    </li>
+  `,
+  data() {
+    return {
+      detailsAreVisible: false,
+      friend: { 
+        id: 'manuel', 
+        name: 'Manuel Lorenz', 
+        phone: '01234 5678 991', 
+        email: 'manuel@localhost.com' 
+      }
     }
   },
   methods: {
